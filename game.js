ec6bc255460844e5b14af24554f9b924a6fc255e
syuhei176 <syuhei176@gmail.com>
2013-05-12 11:02:06 +0900

    テンプレートを修正

--- game.js
+++ game.js
@@ -4,33 +4,13 @@ window.onload = function(){
 
     game.fps = 15;
     game.preload("lib/images/chara1.png");
+    game.preload("lib/images/chara2.png");
     game.onload = function(){
     	
-    	for(var key in classes) {
-        	var m_bear = new classes[key].klass(game);
-        	classes[key].instances.push(m_bear);
-    	}
-    	/*
-        bear.frame = 5;
-        game.rootScene.addChild(bear);
-
-        bear.addEventListener("enterframe", function(){
-            this.x += 1;
-
-            this.frame = this.age % 2 + 6;
-        });
-
-        bear.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bear);
-        });
-        */
+        var m_bear = new classes["Player"].klass(game);
+        classes["Player"].instances.push(m_bear);
     };
 
-    /**
-     * Game#start
-     * ゲームを開始する。この関数を実行するまで、ゲームは待機状態となる。
-     * 代わりに Game#debug を使うことで、デバッグモードで起動することができる。
-     * Game#pause(); で一時停止し、 Game#resume(); で再開することができる。
-     */
     game.start();
+    
 };
--- gameobjects.js
+++ gameobjects.js
@@ -5,39 +5,72 @@
 
 
   
-  function c_player(game) {
+  function c_Player(game) {
         var sprite = new Sprite(32, 32);
         sprite.image = game.assets["lib/images/chara1.png"];
         sprite.x = 0;
         sprite.y = 0;
     game.rootScene.addChild(sprite);
+    function create(name) {
+    	if(classes[name]) {
+        	var m_bear = new classes[name].klass(game);
+        	classes[name].instances.push(m_bear);
+          return m_bear;
+    	}else{
+    		console.error("Error in 「create」");
+    	}
+    }
+    function moveright(d) {
+    	if(!d) d = 1;
+    	sprite.x += d;
+    }
+    function moveleft(d) {
+    	if(!d) d = 1;
+    	sprite.x -= d;
+    }
+    function moveup(d) {
+    	if(!d) d = 1;
+    	sprite.y -= d;
+    }
+    function movedown(d) {
+    	if(!d) d = 1;
+    	sprite.y += d;
+    }
+    function setPosition(x, y) {
+    	sprite.x = x;
+    	sprite.y = y;
+    }
 
 
-  var current_state = "g17pc94gad003";
+  var current_state = "l17qb3apal005";
     
   
     function enterFrame() {
     
       
-     
-      
-    if(current_state == "g17pc94gad003") {
+    if(current_state == "l17qb3apal005") {
+            setPosition(100, 200);
             
-            
-              current_state = "g17pc94gad001";
+              current_state = "l17qb3apal007";
     }
           
      
+     
+      
+     
+      
       
     }
   
     function right() {
     
       
-    if(current_state == "g17pc94gad001") {
-            sprite.x++;
+     
+      
+    if(current_state == "l17qb3apal007") {
+            moveright(1);
             
-              current_state = "g17pc94gad001";
+              current_state = "l17qb3apal007";
     }
           
      
@@ -49,10 +82,30 @@
     function left() {
     
       
-    if(current_state == "g17pc94gad001") {
-            sprite.x--;
+     
+      
+    if(current_state == "l17qb3apal007") {
+            moveleft(1);
             
-              current_state = "g17pc94gad001";
+              current_state = "l17qb3apal007";
+    }
+          
+     
+     
+      
+      
+    }
+  
+    function up() {
+    
+      
+     
+      
+    if(current_state == "l17qb3apal007") {
+            
+            var tama = create("Tama");
+tama.setPosition(sprite.x, sprite.y);
+              current_state = "z17qb4kail001";
     }
           
      
@@ -66,12 +119,17 @@
       
      
       
+     
+      
       
     }
   
 
   sprite.addEventListener("enterframe", function(){
     enterFrame();
+       if(game.input.up) {
+         up();
+       }
        if(game.input.left) {
          left();
        }
@@ -83,11 +141,154 @@
                       return {
                         getSprite : function() {
                           return sprite;
-                        }
+                        },
+                         setPosition : setPosition
                       }
 }
-        window.classes["player"] = {
-                            klass : c_player,
+        window.classes["Player"] = {
+                            klass : c_Player,
+          instances : []};
+
+    
+
+  
+  function c_Tama(game) {
+        var sprite = new Sprite(32, 32);
+        sprite.image = game.assets["lib/images/chara2.png"];
+        sprite.x = 0;
+        sprite.y = 0;
+    game.rootScene.addChild(sprite);
+    function create(name) {
+    	if(classes[name]) {
+        	var m_bear = new classes[name].klass(game);
+        	classes[name].instances.push(m_bear);
+          return m_bear;
+    	}else{
+    		console.error("Error in 「create」");
+    	}
+    }
+    function moveright(d) {
+    	if(!d) d = 1;
+    	sprite.x += d;
+    }
+    function moveleft(d) {
+    	if(!d) d = 1;
+    	sprite.x -= d;
+    }
+    function moveup(d) {
+    	if(!d) d = 1;
+    	sprite.y -= d;
+    }
+    function movedown(d) {
+    	if(!d) d = 1;
+    	sprite.y += d;
+    }
+    function setPosition(x, y) {
+    	sprite.x = x;
+    	sprite.y = y;
+    }
+
+
+  var current_state = "l17qb3apal00b";
+    
+  
+    function enterFrame() {
+    
+      
+    if(current_state == "l17qb3apal00b") {
+            sprite.x = 100;
+sprite.y = 160;
+            sprite.x = 0;
+sprite.y = 0;
+
+              current_state = "l17qb3apal00d";
+    }
+          
+     
+     
+      
+    if(current_state == "l17qb3apal00d") {
+            
+            sprite.y--;
+              current_state = "z17qb4kail005";
+    }
+          
+     
+     
+      
+    if(current_state == "z17qb4kail005") {
+            
+            sprite.y--;
+              current_state = "z17qb4kail005";
+    }
+          
+     
+      
+    }
+  
+    function right() {
+    
+      
+     
+      
+     
+      
+      
+    }
+  
+    function left() {
+    
+      
+     
+      
+     
+      
+      
+    }
+  
+    function up() {
+    
+      
+     
+      
+     
+      
+      
+    }
+  
+    function hitevent() {
+    
+      
+     
+      
+     
+      
+      
+    }
+  
+
+  sprite.addEventListener("enterframe", function(){
+    enterFrame();
+       if(game.input.up) {
+         up();
+       }
+       if(game.input.left) {
+         left();
+       }
+       if(game.input.right) {
+         right();
+       }
+       
+  });
+                      return {
+                        getSprite : function() {
+                          return sprite;
+                        },
+                         setPosition : setPosition
+                      }
+}
+        window.classes["Tama"] = {
+                            klass : c_Tama,
           instances : []};
 
     
